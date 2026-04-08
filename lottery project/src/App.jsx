7d698092a41d853275066325c0f54f4b7b4387e9
satyam shukla <satyamshukla9528@gmail.com>
2026-04-08 23:42:49 +0530

    update new functions

--- lottery project/src/App.jsx
+++ lottery project/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Lottery from "./Lottery";
+import TicketNum from "./TicketNum";
 
 function App() {
   return (
 <>
-<Lottery/>
+<TicketNum num={5}/>
+<TicketNum num={5}/>
+<TicketNum num={5}/>
     </>
   );
 }
--- lottery project/src/Lottery.jsx
+++ lottery project/src/Lottery.jsx
@@ -7,6 +7,9 @@ import { genTicket, sum} from "./helper";
 export default function Lottery(){
     let [ticket, setTicket] = useState(genTicket(3));
     let isWinning = sum(ticket) === 15;
+    let buyTicket =() =>{
+        setTicket(genTicket(3));
+    }
     return(
         <div>
             <h1>Lottery Game!</h1>
@@ -15,6 +18,8 @@ export default function Lottery(){
                 <span>{ticket[1]}</span>
                 <span>{ticket[2]}</span>
             </div>
+            <br></br>
+            <button onClick={buyTicket}>Buy New Ticket</button>
              <h3>{isWinning && "Congratulations, you Won!!"}</h3>
         </div>
     );

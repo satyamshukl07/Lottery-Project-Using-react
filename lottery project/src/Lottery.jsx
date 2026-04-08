dcfdd1cc4136b9960acfce9eb1f52b300159d8e8
satyam shukla <satyamshukla9528@gmail.com>
2026-04-08 11:15:07 +0530

    first commit on Lottry project

---
+++ lottery project/src/Lottery.jsx
@@ -0,0 +1,21 @@
+import {useState} from "react";
+import "./Lottery.css";
+import { genTicket, sum} from "./helper";
+
+
+
+export default function Lottery(){
+    let [ticket, setTicket] = useState(genTicket(3));
+    let isWinning = sum(ticket) === 15;
+    return(
+        <div>
+            <h1>Lottery Game!</h1>
+            <div className="ticket">
+                <span>{ticket[0]}</span>
+                <span>{ticket[1]}</span>
+                <span>{ticket[2]}</span>
+            </div>
+             <h3>{isWinning && "Congratulations, you Won!!"}</h3>
+        </div>
+    );
+}
